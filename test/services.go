1a603e6d1914d1f5517c6430fe85a88d3088cb42
Keon Amini <keon.amini@skael.com>
2021-12-06 14:05:29 -0600

    fixed redis client

--- test/services.go
+++ test/services.go
@@ -3,17 +3,34 @@ package test
 import (
 	"fmt"
 
+	"github.com/sirupsen/logrus"
+
 	"github.com/keon94/go-compose/docker"
 
 	"github.com/go-redis/redis/v7"
 )
 
 func GetRedisClient(container *docker.Container) (interface{}, error) {
-	host, port, err := docker.GetEndpoint(container)
+	host, ports, err := docker.GetAllEndpoints(container)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
-	return redis.NewClient(&redis.Options{
-		Addr: fmt.Sprintf("%s:%s", host, port),
-	}), nil
+	publicPorts := ports["6379"]
+	if len(publicPorts) == 0 {
+		return nil, fmt.Errorf("redis port not found")
+	}
+	var connString string
+	var conn *redis.Client
+	for _, publicPort := range publicPorts {
+		connString = fmt.Sprintf("%s:%s", host, publicPort)
+		conn = redis.NewClient(&redis.Options{Addr: connString})
+		if err == nil {
+			break
+		}
+		logrus.Infof("redis connection \"%s\" failed with %s. trying another if available.", connString, err.Error())
+	}
+	if connString == "" {
+		return nil, fmt.Errorf("no valid redis connection could be establised")
+	}
+	return conn, nil
 }
